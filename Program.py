68af4801168ca63a9aa124b60d97250c832a27e9
Juji <kuntzoliver@gmail.com>
2022-01-26 19:38:28 +0100

    Changed to python, removed cs files

---
+++ Program.py
@@ -0,0 +1,29 @@
+﻿from random import randint
+import time
+
+
+def clear():
+    print("\n" * 80)
+
+def race(mice = 2):
+
+    mice = ['----{,_,">' for i in range(mice)]
+    finished = [False,]
+
+    while not all(finished):
+
+        time.sleep(1)
+        clear()
+
+        print('\t' * 8, '|')
+
+        for i in range(len(mice)):
+
+            x = randint(1, 6)
+            spaces = ' ' * x
+            mice[i] = spaces + mice[i]
+            print(mice[i])
+
+        finished = map(lambda mouse: False if len(mouse) < 50 else True, mice)
+
+race(3)
