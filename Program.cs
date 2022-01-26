cb749e23de9277a6b0718465ef562d7916a97c15
Juji <kuntzoliver@gmail.com>
2022-01-26 12:33:52 +0100

    made all classes

--- Program.cs
+++ Program.cs
@@ -7,3 +7,28 @@ public class RaceManager
 {
 
 }
+
+public class BookMaker
+{
+
+}
+public class Bet
+{
+
+}
+public class Player
+{
+
+}
+public class Track
+{
+
+}
+public class Rat
+{
+
+}
+public class RNG
+{
+
+}
