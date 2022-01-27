a71229b44ef7c1594893fae5ada1ee695f706a8c
Juji <kuntzoliver@gmail.com>
2022-01-27 11:06:06 +0100

    added groups and added numbers to the rats

--- Program.py
+++ Program.py
@@ -2,29 +2,45 @@
 import time
 
 
-
 def clear():
     print("\n" * 80)
+    
+class race:
+    def race(rat, rat2 = 2):
 
-def race(rat = 2):
+        rat = ['1----{,_,">' for i in range(rat)]
+        finished = [False,]
+        
+        rat2 = ['2----{,_,">' for i in range(rat2)]
+        finished = [False,]
+        
 
-    rat = ['----{,_,">' for i in range(rat)]
-    finished = [False,]
+        while not all(finished):
 
-    while not all(finished):
+            time.sleep(1)
+            clear()
 
-        time.sleep(1)
-        clear()
+            print('\t' * 8, 'FINISH')
 
-        print('\t' * 8, '|')
+            for i in range(len(rat)):
+                x = randint(1, 8)
+                spaces = ' ' * x
+                rat[i] = spaces + rat[i]
+                print(rat[i])
+                
+        
+            finished = map(lambda mouse: False if len(mouse) < 50 else True, rat)
+            
+            print("--------------------------------------------------------------------")            
+            
+            for i in range(len(rat2)):
+                x = randint(1, 8)
+                spaces = ' ' * x
+                rat2[i] = spaces + rat2[i]
+                print(rat2[i])
+                
+        
+            finished = map(lambda mouse: False if len(mouse) < 50 else True, rat)
+            
 
-        for i in range(len(rat)):
-
-            x = randint(1, 6)
-            spaces = ' ' * x
-            rat[i] = spaces + rat[i]
-            print(rat[i])
-
-        finished = map(lambda mouse: False if len(mouse) < 50 else True, rat)
-
-race(3)
+    race(3)
