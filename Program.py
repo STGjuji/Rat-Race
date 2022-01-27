409403aff90c9cb729d499b654bf462a062a356d
Juji <kuntzoliver@gmail.com>
2022-01-27 12:44:47 +0100

    added questions

--- Program.py
+++ Program.py
@@ -6,12 +6,12 @@ def clear():
     print("\n" * 80)
     
     
-Question = input("Are you betting on group 1 or 2")
+Question = input("Are you betting on group 1 or 2   ")
 if Question == ("1"):
 	print ("Good Choice!")
 elif Question == ("2"):
 	print ("Good Choice!")
- 
+	
 class race:
     def race(rat, rat2 = 2):
 
@@ -27,10 +27,10 @@ class race:
             time.sleep(1)
             clear()
 
-            print('\t' * 8, 'FINISH')
+            print('\t' * 9, 'FINISH')
 
             for i in range(len(rat)):
-                x = randint(1, 8)
+                x = randint(1, 10)
                 spaces = ' ' * x
                 rat[i] = spaces + rat[i]
                 print(rat[i])
@@ -41,13 +41,17 @@ class race:
             print("--------------------------------------------------------------------")            
             
             for i in range(len(rat2)):
-                x = randint(1, 8)
+                x = randint(1, 10)
                 spaces = ' ' * x
                 rat2[i] = spaces + rat2[i]
                 print(rat2[i])
                 
         
-            finished = map(lambda mouse: False if len(mouse) < 50 else True, rat)
+            finished = map(lambda mouse: False if len(mouse) < 50 else True, rat2)
             
-
     race(3)
+    
+if Question == ("1"):
+	print ("Good Choice!")
+elif Question == ("2"):
+	print ("Good Choice!")
